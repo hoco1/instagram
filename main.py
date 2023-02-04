24d1affdffd7925eece199a95ccc71dda6b76f21
hoco1 <mohammadrezanorouzi600@gmail.com>
2023-02-04 15:20:21 +0330

    get usr id

--- main.py
+++ main.py
@@ -57,7 +57,51 @@ def add_user(instagram:InstagramSchema):
     return {'cookie':cookie}
 
 # get followers list
-app.get("/instagram/{usr}",tags=['instagram'])
+@app.get("/instagram/{usr}",tags=['instagram'])
 def list_followers(usr:str):
-    pass
+    cookie = serializeList(conn.local.cookies.find().sort('_id',-1).limit(1))[0]
+    del cookie['_id']
+    
+    cookies = {
+        'ig_did': cookie['ig_did'],
+        'mid': cookie['mid'],
+        'rur': cookie['rur'],
+        'csrftoken': cookie['csrftoken'],
+        'ds_user_id': cookie['ds_user_id'],
+        'sessionid': cookie['sessionid'],
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'X-CSRFToken': cookie['csrftoken'],
+        # Static, and I don't know what it is yet (X-IG-App-ID,X-ASBD-ID)
+        'X-IG-App-ID': '936619743392459',
+        'X-ASBD-ID': '198387',
+        #'X-IG-WWW-Claim': 'hmac.AR2t23cd-PLsVw8GByCHBcLMYfsKEk5T9x80YB-dnKMcZOPw',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive',
+        'Referer': 'https://www.instagram.com/ataullah1385/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-GPC': '1',
+        'TE': 'trailers',
+    }
+
+    params_info = (
+        ('username', 'ataullah1385'),
+    )
+
+    response = requests.get('https://www.instagram.com/api/v1/users/web_profile_info/', headers=headers, params=params_info, cookies=cookies)
+    
+    print(response)
+    
+    data = json.loads(response.text)
+    print(data['data']['user']['id'])
+    
+    
+    
 
